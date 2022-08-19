5e7981fb1e2c153003719927e5b25e5998ac0881
marzec <gregmarzec02@gmail.com>
2022-08-18 23:08:49 -0400

    added single thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -6,22 +6,27 @@ module.exports = {
       .then((thoughts) => res.json(thoughts))
       .catch((err) => res.status(500).json(err));
   },
+  getThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtID })
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+  },
   createThought(req, res) {
     Thought.create(req.body)
       .then((thoughts) => {
         return User.findOneAndUpdate(
-          { username: req.body.userID },
+          { username: req.body.username },
           { $push: { thoughts: thoughts._id } },
           { new: true }
         );
       })
-      .then((user) => {
-        if (user) {
-          res.json({ message: "created thought successfully" });
-        } else {
-          res.status(404).json({ message: "error creating user" });
-        }
-      })
+      .then((user) =>
+        !user
+          ? res.status(404).json({
+              message: "Error Creating Thought!",
+            })
+          : res.json("Created the thought ")
+      )
       .catch((err) => {
         console.log(err);
         res.status(500).json(err);
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -2,9 +2,17 @@ const router = require("express").Router();
 
 const {
   getThoughts,
+  getThought,
   createThought,
 } = require("../../controllers/thoughtController");
 
+//get all thoughts / create thought
 router.route("/").get(getThoughts).post(createThought);
+
+router.route("/:thoughtID").get(getThought);
+
+router.route("/:thoughtID");
+
+router.route("/:thoughtID");
 
 module.exports = router;
