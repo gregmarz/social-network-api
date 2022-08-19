855d51359d081a513bcbc8fd1d2c0fbadaf44acf
marzec <gregmarzec02@gmail.com>
2022-08-18 20:23:53 -0400

    added post, and get single user

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,12 +7,17 @@ module.exports = {
       .catch((err) => res.status(500).json(err));
   },
   getSingleUser(req, res) {
-    User.findOne({ _id: req.params.userId })
+    User.findOne({ _id: req.params.userID })
       .then((user) =>
         !user
           ? res.status(404).json({ message: "No user with that ID" })
           : res.json(user)
       )
+      .catch((err) => res.status(500).json(err));
+  },
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
       .catch((err) => res.status(500).json(err));
   },
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,7 +1,13 @@
 const router = require("express").Router();
 
-const { getUsers } = require("../../controllers/userController");
+const {
+  getUsers,
+  createUser,
+  getSingleUser,
+} = require("../../controllers/userController");
 
-router.route("/").get(getUsers);
+router.route("/").get(getUsers).post(createUser);
+
+router.route("/:userID").get(getSingleUser);
 
 module.exports = router;
